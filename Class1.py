bfdfcf72ac7e09c62f05f29149bca61c7648bc40
wlin114 <wlin114@outlook.com>
2016-09-10 13:27:21 -0400

    First Class

---
+++ Class1.py
@@ -0,0 +1,104 @@
+
+# "my name is Wayne"
+
+# 1
+# -1
+# 0
+
+# 1.1
+# -1.1
+# 0.0
+
+# True
+# False
+
+# a = str
+# b = int
+# c = float
+# d = bool
+
+# e = "My name is Wayne"
+# f = 1
+# g = 1.1
+# h = True
+
+# str1 = "Hello"
+# str2 = "World"
+# str3 = str1 + str2
+# print(str3)
+
+# a = "Hello"
+# b = a
+# print(b)
+
+# c = "Hello"
+# d = 1
+# print(d)
+
+# print(type(d))
+
+# c = 1 + 1
+# d = 1 - 1
+# e = 2 * 2
+# f = 3 / 2
+# print(c, d, e, f)
+
+# print(1.0 + 2.5)
+# print(3.5 - 1.111)
+# print(8.8 * 8.88)
+# print(100.1 / 1.1)
+
+# a = True
+# b = False
+
+
+
+# if a.isdigit():
+# 	b = int(a)
+# 	if b % 2 == 0:
+# 		print("Your enter an even number")
+# 	else:
+# 		print("Your enter an odd number")
+# else:
+# 	print("Invalid input")
+
+# import sys
+# a = input("Please enter a number: ")
+
+# while a.isdigit() == False:
+# 	print("Invalid Input")
+# 	a = input("Please enter a number: ")
+
+# b = int(a)
+# if b % 2 == 0:
+# 	print("Your enter an even number")
+# else:
+# 	print("Your enter an odd number")
+
+
+# for i in range(1, 10):
+# 	print(i)
+
+# for i in "abc":
+# 	print(i)
+
+# import sys
+
+# a = input()
+
+# counter = 0
+
+
+# a.isalpha()
+
+# for i in a:
+# 	print(i)
+# 	counter = counter + 1
+
+# print("Your enter ", counter, " character(s)")
+
+
+
+
+
+
